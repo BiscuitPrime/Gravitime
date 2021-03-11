5a2a96964114634b2bf8e8387e097afec05b3d41
BiscuitPrime <74375728+BiscuitPrime@users.noreply.github.com>
2021-03-11 17:25:10 +0100

    Add files via upload
    Laser et boîte optimisées et qui marchent

--- Source/Entities/Script/Laser.gd
+++ Source/Entities/Script/Laser.gd
@@ -1,19 +1,21 @@
 extends Area2D
 class_name Laser
+#Ce script est utilisé par les lasers qui servent pour les puzzles
 
-onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+onready var anim_player: AnimationPlayer = get_node("AnimationPlayer") #variable qui va contenir l'animation du laser
 
+#Cette fonction est appelée lorsque le laser est chargé :
 func _ready():
 	anim_player.play_backwards("shutdown")
-	get_node("CollisionShape2D").set_disabled(false)
+	collision_layer=4
 
 func _on_DoorButton_body_entered(body: Node) -> void:
 	anim_player.play("shutdown")
-	get_node("CollisionShape2D").set_disabled(true)
+	collision_layer=0
 	
 func _on_DoorButton_body_exited(body: Node) -> void:
 	anim_player.play_backwards("shutdown")
-	get_node("CollisionShape2D").set_disabled(false)
+	collision_layer=4
 
 #Cette fonction est appelée quand un corps étranger rentre dans le laser
 #Si le corps étranger a une méthode hit, on l'appelle
