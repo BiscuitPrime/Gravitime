0749a21297d2746da45089a87f47226304a0d9e2
BiscuitPrime <74375728+BiscuitPrime@users.noreply.github.com>
2021-05-14 17:12:30 +0200

    Revert "Add files via upload"

--- Source/Actors/Script/Enemy.gd
+++ Source/Actors/Script/Enemy.gd
@@ -7,14 +7,15 @@ onready var DetectorRight = $DetectorRight #Détecte ce qui est en face de lui (
 onready var DetectorLeft = $DetectorLeft #Détecte ce qui est en face de lui (entité et mur)
 onready var DetectorPlatRight = $DetectorPlatformRight #Détecte ce qui est sous lui (plateformes)
 onready var DetectorPlatLeft = $DetectorPlatformLeft #Détecte ce qui est sous lui (plateformes)
-
+onready var _animation_player = $AnimationPlayer
+onready var sprite = $enemy
 var timeposition #variable qui va contenir la position de l'ennemi avant la manipulation temporelle
 
 #Cette fonction est appelée lorsque l'instance de l'ennemi est loadée
 func _ready():
 	add_to_group("timecontrol") #On l'ajoute au groupe timecontrol, ce qui indique que le temps va l'affecter
 	_velocity.x = -speed.x #On donne une vitesse initiale à l'ennemi
-
+	_animation_player.play("roule")
 #Cette fonction est appelée à chaque frame du jeu 
 func _physics_process(delta: float) -> void:
 	if is_in_gravity_field:
@@ -26,12 +27,12 @@ func _physics_process(delta: float) -> void:
 func calculate_new_velocity(_velocity):
 	if not DetectorPlatRight.is_colliding() : #Si le détecteur ne détecte pas de plateforme sous l'ennemi, il rebrousse chemin
 		_velocity.x=-speed.x
+		sprite.flip_h = not sprite.flip_h
 	elif not DetectorPlatLeft.is_colliding():
 		_velocity.x=speed.x
 	elif DetectorLeft.is_colliding() or DetectorRight.is_colliding(): #Si l'ennemi détecte un mur, il rebrousse chemin
 		_velocity.x *= -1.0
-	if _velocity.x==0 :
-		_velocity.x=-speed.x
+		sprite.flip_h = not sprite.flip_h
 	return _velocity
 
 #Cette fonction est appelée lorsque l'ennemi est touché par l'attaque du joueur
