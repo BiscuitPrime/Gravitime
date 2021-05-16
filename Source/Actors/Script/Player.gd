69d33ad9fb785f38f09775ab426c47763f238ccc
BiscuitPrime <74375728+BiscuitPrime@users.noreply.github.com>
2021-05-16 19:23:42 +0200

    Add files via upload
    bug fixe ?

--- Source/Actors/Script/Player.gd
+++ Source/Actors/Script/Player.gd
@@ -59,6 +59,7 @@ func get_direction() -> Vector2:
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.get_action_strength("jump") and is_on_floor() else 1.0
 	)
+
 #Fonction qui permet de lancer au bon moment les animations de déplacements
 func movements_animation(direction: Vector2) -> void:
 	var anim_actuelle = _animation_player.get_current_animation() 
