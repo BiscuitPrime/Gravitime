2c7be10d42ef1b578a7381c4e501ae88149af875
MartialHuot <martial.huot@telecom-sudparis.eu>
2021-05-16 23:04:01 +0100

    Correction test unit for entities

--- test/unit/test_box_unit.gd
+++ test/unit/test_box_unit.gd
@@ -5,5 +5,6 @@ var box = preload("res://Source/Entities/Script/Box.gd").new() #On load le scrip
 
 #On teste si la méthode save() de la boîte fonctionne : (elle doit enregistrer une position)
 func test_save():
+	box.position = Vector2(45,56)
 	box.save()
-	assert_eq(null, box.timeposition, "Should fail : timeposition != null (la boîte a enregistré une position)")
+	assert_eq(box.timeposition, Vector2(0,0), "Should fail : timeposition != null (la boîte a enregistré une position)")
--- test/unit/test_explosiveBox_unit.gd
+++ test/unit/test_explosiveBox_unit.gd
@@ -5,5 +5,6 @@ var explosiveBox = preload("res://Source/Entities/Script/Box.gd").new() #On load
 
 #On teste si la méthode save() de la boîte fonctionne : (elle doit enregistrer une position)
 func test_save():
+	explosiveBox.position = Vector2(45,56)
 	explosiveBox.save()
-	assert_eq(null, explosiveBox.timeposition, "Should fail : timeposition != null (la boîte a enregistré une position)")
+	assert_eq(explosiveBox.timeposition, Vector2(0,0), "Should fail : timeposition != null (la boîte a enregistré une position)")
