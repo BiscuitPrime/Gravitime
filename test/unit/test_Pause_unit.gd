112e81d7e70cb668108ca6d6dd8edf09aec70acb
BiscuitPrime <74375728+BiscuitPrime@users.noreply.github.com>
2021-05-21 20:12:09 +0200

    Delete test directory

--- test/unit/test_Pause_unit.gd
+++
@@ -1,6 +0,0 @@
-extends "res://addons/gut/test.gd"
-var Pause = preload("res://Source/UserInterface/Script/Pause.gd").new()
-func test_read():
-	Pause.visible = true
-	Pause._read()
-	assert_eq(Pause.visible, false, "Should succeed: visible doit être false après l'application de la fonction")
